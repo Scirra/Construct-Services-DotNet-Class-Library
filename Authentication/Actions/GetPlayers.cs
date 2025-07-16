e1e38b8adb728c85bb4fa5db245e63af4ef2bc8f
Thomas Gullen <tom@scirra.com>
2025-07-16 13:47:17 +0100

    Support for get players by player ID(s)

--- Authentication/Actions/GetPlayers.cs
+++ Authentication/Actions/GetPlayers.cs
@@ -1,13 +1,16 @@
-﻿using ConstructServices.Authentication.Enums;
+﻿using System;
+using ConstructServices.Authentication.Enums;
 using ConstructServices.Authentication.Responses;
 using ConstructServices.Leaderboards;
 using System.Collections.Generic;
 using System.Threading.Tasks;
+using JetBrains.Annotations;
 
 namespace ConstructServices.Authentication.Actions;
 
 public static partial class Players
 {
+    [UsedImplicitly]
     public static GetPlayersResponse GetPlayers(
         this AuthenticationService service,
         PlayerOrdering ordering,
@@ -29,6 +32,29 @@ public static partial class Players
 
         formData.Add("order", ordering.ToString());
 
+        return Task.Run(() => Request.ExecuteAuthenticationRequest<GetPlayersResponse>(
+            path,
+            service,
+            formData
+        )).Result;
+    }
+    
+    [UsedImplicitly]
+    public static GetPlayersResponse GetPlayers(
+        this AuthenticationService service,
+        Guid playerID)
+        => GetPlayers(service, [playerID]);
+
+    [UsedImplicitly]
+    public static GetPlayersResponse GetPlayers(
+        this AuthenticationService service,
+        List<Guid> playerIDs)
+    {
+        const string path = "/getplayers.json";
+        var formData = new Dictionary<string, string>
+        {
+            { "playerIDs", string.Join(",", playerIDs) }
+        };
         return Task.Run(() => Request.ExecuteAuthenticationRequest<GetPlayersResponse>(
             path,
             service,
