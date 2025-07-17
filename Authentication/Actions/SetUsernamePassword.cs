a7b17fe26b678b21d8a16742eaef71e6cd652b1d
Thomas Gullen <tom@scirra.com>
2025-07-17 10:35:52 +0100

    Add support for set username + password end point

---
+++ Authentication/Actions/SetUsernamePassword.cs
@@ -0,0 +1,29 @@
+﻿using ConstructServices.Common;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace ConstructServices.Authentication.Actions;
+
+public static partial class Players
+{
+    public static BaseResponse SetUsernameAndPassword(
+        this AuthenticationService service,
+        Guid playerID,
+        string username,
+        string password)
+    {
+        const string path = "/setusernamepassword.json";
+
+        return Task.Run(() => Request.ExecuteAuthenticationRequest<BaseResponse>(
+            path,
+            service,
+            new Dictionary<string, string>
+            {
+                { "playerID", playerID.ToString() },
+                { "username", username },
+                { "password", password }
+            }
+        )).Result;
+    }
+}
