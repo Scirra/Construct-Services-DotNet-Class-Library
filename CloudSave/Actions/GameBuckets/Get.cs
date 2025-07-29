48e93eb486f0e6612d800fd151a9dae6f9fa4bec
Thomas Gullen <tom@scirra.com>
2025-07-29 12:53:54 +0100

    Implement get bucket end point

--- CloudSave/Actions/GameBuckets/Get.cs
+++ CloudSave/Actions/GameBuckets/Get.cs
@@ -1,9 +1,31 @@
-﻿namespace ConstructServices.CloudSave.Actions;
+﻿using System;
+using ConstructServices.CloudSave.Responses;
+using ConstructServices.Common;
+using JetBrains.Annotations;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace ConstructServices.CloudSave.Actions;
 
 public static partial class GameBuckets
 {
-    public static void Get()
+    /// <summary>
+    /// Get a bucket by its ID.  Doesn't return cloud saves in bucket, just the bucket itself.
+    /// </summary>
+    [UsedImplicitly]
+    public static BucketResponse Get(
+        this CloudSaveService service,
+        Guid bucketID)
     {
-
+        var formData = new Dictionary<string, string>
+        {
+            { "bucketID", bucketID.ToString() }
+        };
+        const string path = "/getbucket.json";
+        return Task.Run(() => Request.ExecuteRequest<BucketResponse>(
+            path,
+            service,
+            formData
+        )).Result;
     }
 }
