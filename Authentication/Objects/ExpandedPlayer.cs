829a35136c4d8e813620c8f64ba7b482f3c895b1
Thomas Gullen <tom@scirra.com>
2025-11-26 13:48:38 +0000

    Make new expanded player fields public

--- Authentication/Objects/ExpandedPlayer.cs
+++ Authentication/Objects/ExpandedPlayer.cs
@@ -56,16 +56,16 @@ public sealed class ExpandedPlayer
     public long TotalCloudSavesSize { get; set; }
         
     [JsonProperty(PropertyName = "totalPrivateCloudSaves")]
-    internal int TotalPrivateCloudSaves { get; set; }
+    public int TotalPrivateCloudSaves { get; set; }
 
     [JsonProperty(PropertyName = "totalPrivateCloudSavesSize")]
-    internal long TotalPrivateCloudSavesSize { get; set; }
+    public long TotalPrivateCloudSavesSize { get; set; }
 
     [JsonProperty(PropertyName = "totalBucketCloudSaves")]
-    internal int TotalBucketCloudSaves { get; set; }
+    public int TotalBucketCloudSaves { get; set; }
 
     [JsonProperty(PropertyName = "totalBucketCloudSavesSize")]
-    internal long TotalBucketCloudSavesSize { get; set; }
+    public long TotalBucketCloudSavesSize { get; set; }
 
     [JsonProperty(PropertyName = "totalRatings")]
     public int TotalRatings { get; set; }
