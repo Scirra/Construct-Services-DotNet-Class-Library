e44be8e9efc48eab5e837ab79b316091e570a48c
Thomas Gullen <tom@scirra.com>
2025-11-27 15:00:12 +0000

    Implemented delete channel end point

---
+++ Broadcasts/Actions/Delete.cs
@@ -0,0 +1,31 @@
+﻿using ConstructServices.Common;
+using JetBrains.Annotations;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace ConstructServices.Broadcasts.Actions;
+
+public static class Delete
+{
+    /// <summary>
+    /// Create a new channel
+    /// </summary>
+    [UsedImplicitly]
+    public static BaseResponse DeleteChannel(
+        this BroadcastService service,
+        Guid channelID)
+    {
+        var formData = new Dictionary<string, string>
+        {
+            { "channelID", channelID.ToString() }
+        };
+
+        const string path = "/deletechannel.json";
+        return Task.Run(() => Request.ExecuteRequest<BaseResponse>(
+            path,
+            service,
+            formData
+        )).Result;
+    }
+}
