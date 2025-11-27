41acf6a84ef230cd2a5522338b4a0f74ab9fa303
Thomas Gullen <tom@scirra.com>
2025-11-27 15:51:05 +0000

    Add get channel action

--- Broadcasts/Actions/GetChannel.cs
+++ Broadcasts/Actions/GetChannel.cs
@@ -1,20 +1,22 @@
-﻿using System.Collections.Generic;
+﻿using ConstructServices.Broadcasts.Responses;
+using JetBrains.Annotations;
+using System;
+using System.Collections.Generic;
 using System.Globalization;
 using System.Threading.Tasks;
-using ConstructServices.Broadcasts.Responses;
 using ConstructServices.Common;
-using JetBrains.Annotations;
 
 namespace ConstructServices.Broadcasts.Actions;
 
 public static partial class Get
-{
+{    
     /// <summary>
     /// Get all channels in this game
     /// </summary>
     [UsedImplicitly]
-    private static ChannelsResponse DoGetChannels(
+    private static ChannelResponse DoGetChannel(
         BroadcastService service,
+        Guid channelID,
         string requestedLanguage,
         CultureInfo culture)
     {
@@ -27,9 +29,9 @@ public static partial class Get
         {
             formData.Add("culture", culture.ToString());
         }
-
-        const string path = "/getchannels.json";
-        return Task.Run(() => Request.ExecuteRequest<ChannelsResponse>(
+        
+        const string path = "/getchannel.json";
+        return Task.Run(() => Request.ExecuteRequest<ChannelResponse>(
             path,
             service,
             formData
@@ -42,29 +44,29 @@ public static partial class Get
         /// Get all channels in this game
         /// </summary>
         [UsedImplicitly]
-        public ChannelsResponse GetChannels()
-            => DoGetChannels(service, null, null);
+        public ChannelResponse GetChannel(Guid channelID)
+            => DoGetChannel(service, channelID, null, null);
         
         /// <summary>
         /// Get all channels in this game returning the translatable elements as requestedLanguage if possible.
         /// </summary>
         [UsedImplicitly]
-        public ChannelsResponse GetChannels(string requestedLanguage)
-            => DoGetChannels(service, requestedLanguage, null);
+        public ChannelResponse GetChannel(Guid channelID, string requestedLanguage)
+            => DoGetChannel(service, channelID, requestedLanguage, null);
         
         /// <summary>
         /// Get all channels in this game returning the translatable elements as requestedLanguage if possible
         /// and data returned formatted to a specified culture.
         /// </summary>
         [UsedImplicitly]
-        public ChannelsResponse GetChannels(string requestedLanguage, CultureInfo inCulture)
-            => DoGetChannels(service, requestedLanguage, inCulture);
+        public ChannelResponse GetChannel(Guid channelID, string requestedLanguage, CultureInfo inCulture)
+            => DoGetChannel(service, channelID, requestedLanguage, inCulture);
 
         /// <summary>
         /// Get all channels in this game with data returned formatted to a specified culture.
         /// </summary>
         [UsedImplicitly]
-        public ChannelsResponse GetChannels(CultureInfo inCulture)
-            => DoGetChannels(service, null, inCulture);
+        public ChannelResponse GetChannel(Guid channelID, CultureInfo inCulture)
+            => DoGetChannel(service, channelID, null, inCulture);
     }
 }
