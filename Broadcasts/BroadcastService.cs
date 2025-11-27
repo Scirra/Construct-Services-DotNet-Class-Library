e028878c32148815e08f734047bc3f1ed16d6382
Thomas Gullen <tom@scirra.com>
2025-11-27 13:58:40 +0000

    Add new broadcast service + config

---
+++ Broadcasts/BroadcastService.cs
@@ -0,0 +1,31 @@
+﻿using ConstructServices.Common;
+using JetBrains.Annotations;
+using System;
+using System.Collections.Generic;
+
+namespace ConstructServices.Broadcasts;
+
+[UsedImplicitly]
+public class BroadcastService : BaseService
+{
+    /// <summary>
+    /// Create a new instance of the broadcast service
+    /// </summary>
+    /// <param name="gameID">Game ID service is for</param>
+    /// <param name="aPIKey">API key</param>
+    public BroadcastService(Guid gameID, string aPIKey) : base(gameID, Config.APIDomain, aPIKey)
+    {
+    }
+
+    /// <summary>
+    /// Create a new instance of the broadcast service
+    /// </summary>
+    /// <param name="gameID">Game ID service is for</param>
+    public BroadcastService(Guid gameID) : base(gameID, Config.APIDomain)
+    {
+    }
+
+    internal override void AddServiceSpecificFormData(Dictionary<string, string> formData)
+    {
+    }
+}
---
+++ Broadcasts/Config.cs
@@ -0,0 +1,7 @@
+﻿namespace ConstructServices.Broadcasts;
+
+internal static class Config
+{
+    internal static string APIDomain => 
+        !GlobalConfig.DevMode ? "https://broadcasts.construct.net" : "https://broadcasts.constructdev.net";
+}
