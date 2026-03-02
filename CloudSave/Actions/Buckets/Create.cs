20ac21a79be96151c529e6e16c2c428d63608e89
Thomas Gullen <tom@scirra.com>
2026-03-02 09:41:50 +0000

    Update to new API design

--- CloudSave/Actions/Buckets/Create.cs
+++ CloudSave/Actions/Buckets/Create.cs
@@ -1,8 +1,7 @@
-﻿using ConstructServices.CloudSave.Enums;
+﻿using ConstructServices.CloudSave.Objects;
 using ConstructServices.CloudSave.Responses;
 using ConstructServices.Common;
 using JetBrains.Annotations;
-using System.Collections.Generic;
 using System.Threading.Tasks;
 
 namespace ConstructServices.CloudSave.Actions;
@@ -15,42 +14,12 @@ public static partial class Buckets
         /// Create a new bucket
         /// </summary>
         [UsedImplicitly]
-        public BucketResponse CreateBucket(CloudSaveGameBucketAccessMode accessMode,
-            string bucketName,
-            bool allowRatings = true,
-            uint? maxBlobs = null,
-            uint? maxBlobSizeBytes = null,
-            uint? maxBlobsPerPlayer = null)
+        public BucketResponse CreateBucket(CreateBucketOptions createBucketOptions)
         {
-            var nameValidator = Common.Validations.BucketName.ValidateName(bucketName);
-            if (!nameValidator.Successfull)
-            {
-                return new BucketResponse(nameValidator.ErrorMessage);
-            }
-
-            var formData = new Dictionary<string, string>
-            {
-                { "bucketName", bucketName },
-                { "accessMode", accessMode.ToString() },
-                { "allowRatings", allowRatings.ToInt().ToString() }
-            };
-            if (maxBlobs.HasValue)
-            {
-                formData.Add("maxBlobs", maxBlobs.Value.ToString());
-            }
-            if (maxBlobSizeBytes.HasValue)
-            {
-                formData.Add("maxBlobSize", maxBlobSizeBytes.Value.ToString());
-            }
-            if (maxBlobsPerPlayer.HasValue)
-            {
-                formData.Add("maxBlobsPerPlayer", maxBlobsPerPlayer.Value.ToString());
-            }
-
             return Request.ExecuteSyncRequest<BucketResponse>(
                 Config.EndPointPaths.Buckets.Create,
                 service,
-                formData
+                createBucketOptions.BuildFormData()
             );
         }
 
@@ -58,42 +27,12 @@ public static partial class Buckets
         /// Create a new bucket
         /// </summary>
         [UsedImplicitly]
-        public async Task<BucketResponse> CreateBucketAsync(CloudSaveGameBucketAccessMode accessMode,
-            string bucketName,
-            bool allowRatings = true,
-            uint? maxBlobs = null,
-            uint? maxBlobSizeBytes = null,
-            uint? maxBlobsPerPlayer = null)
+        public async Task<BucketResponse> CreateBucketAsync(CreateBucketOptions createBucketOptions)
         {
-            var nameValidator = Common.Validations.BucketName.ValidateName(bucketName);
-            if (!nameValidator.Successfull)
-            {
-                return new BucketResponse(nameValidator.ErrorMessage);
-            }
-
-            var formData = new Dictionary<string, string>
-            {
-                { "bucketName", bucketName },
-                { "accessMode", accessMode.ToString() },
-                { "allowRatings", allowRatings.ToInt().ToString() }
-            };
-            if (maxBlobs.HasValue)
-            {
-                formData.Add("maxBlobs", maxBlobs.Value.ToString());
-            }
-            if (maxBlobSizeBytes.HasValue)
-            {
-                formData.Add("maxBlobSize", maxBlobSizeBytes.Value.ToString());
-            }
-            if (maxBlobsPerPlayer.HasValue)
-            {
-                formData.Add("maxBlobsPerPlayer", maxBlobsPerPlayer.Value.ToString());
-            }
-
             return await Request.ExecuteAsyncRequest<BucketResponse>(
                 Config.EndPointPaths.Buckets.Create,
                 service,
-                formData
+                createBucketOptions.BuildFormData()
             );
         }
     }
---
+++ CloudSave/Objects/GameBucketAPIObject.cs
@@ -0,0 +1,62 @@
+﻿using ConstructServices.CloudSave.Enums;
+using ConstructServices.Common;
+using JetBrains.Annotations;
+using System.Collections.Generic;
+
+namespace ConstructServices.CloudSave.Objects;
+
+public abstract class ModifyBucketBase
+{        
+    [UsedImplicitly]
+    public string Name { get; set; }
+
+    [UsedImplicitly]
+    public CloudSaveGameBucketAccessMode AccessMode { get; set; }
+
+    [UsedImplicitly]
+    public bool AllowRatings { get; set; }
+
+    [UsedImplicitly]
+    public uint? MaxBlobs { get; set; }
+
+    [UsedImplicitly]
+    public uint? MaxBlobSizeBytes { get; set; }
+
+    [UsedImplicitly]
+    public uint? MaxBlobsPerPlayer { get; set; }
+
+    protected Dictionary<string, string> BuildBaseFormData() => new()
+    {
+        { "bucketName", Name },
+        { "accessMode", AccessMode.ToString() },
+        { "allowRatings", AllowRatings.ToInt().ToString() },
+        { "maxBlobs", MaxBlobs.ToString() },
+        { "maxBlobSize", MaxBlobSizeBytes.ToString() },
+        { "maxBlobsPerPlayer", MaxBlobsPerPlayer.ToString() }
+    };
+}
+public sealed class CreateBucketOptions : ModifyBucketBase
+{
+    public CreateBucketOptions(
+        string name,
+        CloudSaveGameBucketAccessMode accessMode,
+        bool allowRatings = true,
+        uint? maxBlobs = null,
+        uint? maxBlobSizeBytes = null,
+        uint? maxBlobsPerPlayer = null)
+    {
+        Name = name;
+        AccessMode = accessMode;
+        AllowRatings = allowRatings;
+        MaxBlobs = maxBlobs;
+        MaxBlobSizeBytes = maxBlobSizeBytes;
+        MaxBlobsPerPlayer = maxBlobsPerPlayer;
+    }
+
+    [UsedImplicitly]
+    internal Dictionary<string, string> BuildFormData()
+    {
+        var formData = BuildBaseFormData();
+        return formData;
+    }
+}
