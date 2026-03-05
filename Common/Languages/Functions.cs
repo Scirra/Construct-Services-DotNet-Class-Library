41dbe88e8ee330361530641a54cb2afea8841227
Thomas Gullen <tom@scirra.com>
2026-03-05 14:53:49 +0000

    Add method to get ISO

---
+++ Common/Languages/Functions.cs
@@ -0,0 +1,12 @@
+﻿using JetBrains.Annotations;
+
+namespace ConstructServices.Common.Languages;
+
+[UsedImplicitly]
+public static class Functions
+{
+    public static string ISO(this TargetLanguage targetLanguage)
+        => targetLanguage.GetAttribute<LanguageAttribute>().ISO;
+    public static string ISO(this SourceLanguage sourceLanguage)
+        => sourceLanguage.GetAttribute<LanguageAttribute>().ISO;
+}
