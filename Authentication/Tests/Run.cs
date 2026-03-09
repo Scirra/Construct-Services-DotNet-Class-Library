147e69b1ab51e677ccb4d9fadc80f1fb178cd14f
Thomas Gullen <tom@scirra.com>
2026-03-09 13:55:23 +0000

    Add missing session key param

--- Authentication/Tests/Run.cs
+++ Authentication/Tests/Run.cs
@@ -7,6 +7,7 @@ using System.Diagnostics;
 using System.Linq;
 using ConstructServices.Authentication.Actions;
 using ConstructServices.Authentication.Enums;
+using ConstructServices.Authentication.Objects;
 
 namespace ConstructServices.Authentication.Tests;
 
@@ -14,20 +15,31 @@ public static class Run
 {
     private enum AuthTest
     {
+        Login,
+        LoginPoll,
+        Link,
+        LinkPoll,
+        GetSession,
+        RefreshSession,
+        EndSession,
+
         CreatePlayer,
+        ListNewestPlayers,
+        ListMostRecentlyActivePlayers,
+        GetPlayerByID,
+        GetPlayerByPlayerName,
         ChangePlayerName,
+        GetPlayerByUpdatedName,
         SetUsernamePassword,
-        ChangePassword,
-        ChangeUsername,
-        GetPlayer,
-        ListPlayers,
+        SetUsername,
+        SetPassword,
         ForgottenPassword,
         SetEmailAddress,
         SetRestrictions,
+        SetAvatarByURL,
+        SetAvatarByBase64,
+        DeleteAvatar,
         DeletePlayer,
-
-        SetAvatar,
-        DeleteAvatar
     }
 
     [UsedImplicitly]
@@ -39,99 +51,251 @@ public static class Run
         var sw = new Stopwatch();
         sw.Start();
 
+        // CREATE NEW PLAYER
+        var playerName = "Test" + new Random().Next(0, 10000);
         var createResult = service.RegisterPlayer(new Players.RegisterPlayerOptions
         {
-            PlayerName = "Test" + new Random().Next(0, 10000)
+            PlayerName = playerName
         });
         results[nameof(AuthTest.CreatePlayer)] = new TestResult(createResult, sw);
 
         if (createResult.Success)
         {
             var player = createResult.Player;
+
+            var playerService = new PlayerAuthenticationService(gameID, new SessionKey(createResult.Session.Key));
+            
+            // LOGIN
             {
+                const string testName = nameof(AuthTest.Login);
                 sw.Restart();
-                var result = service.ListPlayers(new Players.ListPlayersOptions
-                {
-                    Ordering = PlayerOrdering.Newest
-                }, new PaginationOptions(1, 20));
-                results[nameof(AuthTest.ListPlayers)] = new TestResult(result, sw);
+                var result = playerService.Login(LoginProvider.Discord);
+                results[testName] = new TestResult(result, sw);
                 if (result.Success)
                 {
-                    if (result.Players.All(c => c.ID != player.ID))
+                    var pollToken = result.PollToken;
+
+                    // LOGIN POLL
                     {
-                        results[nameof(AuthTest.ListPlayers)] =
-                            new TestResult(TestResultStatus.Failed, sw, "Player ID not returned.");
+                        const string pollTestName = nameof(AuthTest.LoginPoll);
+                        sw.Restart();
+                        var pollResult = playerService.LoginPoll(pollToken);
+                        results[pollTestName] = new TestResult(pollResult, sw);
+                    }
+                }
+            }
+            
+            // LINK
+            {
+                const string testName = nameof(AuthTest.Link);
+                sw.Restart();
+                var result = playerService.LinkLoginProvider(LoginProvider.Apple);
+                results[testName] = new TestResult(result, sw);
+                if (result.Success)
+                {
+                    var pollToken = result.PollToken;
+
+                    // LINK POLL
+                    {
+                        const string pollTestName = nameof(AuthTest.LinkPoll);
+                        sw.Restart();
+                        var pollResult = playerService.LinkPoll(pollToken);
+                        results[pollTestName] = new TestResult(pollResult, sw);
                     }
                 }
             }
 
+            // GET SESSION
             {
+                const string testName = nameof(AuthTest.GetSession);
                 sw.Restart();
-                var result = service.ChangePlayerName(player.ID, "Test" + new Random().Next(0, 10000));
-                results[nameof(AuthTest.ChangePlayerName)] = new TestResult(result, sw);
+                var result = playerService.GetSession();
+                results[testName] = new TestResult(result, sw);
             }
 
+            // REFRESH SESSION
             {
+                const string testName = nameof(AuthTest.RefreshSession);
                 sw.Restart();
-                var result = service.SetUsernameAndPassword(player.ID, "Test" + new Random().Next(0, 10000), "abc123#AB£");
-                results[nameof(AuthTest.SetUsernamePassword)] = new TestResult(result, sw);
+                var result = playerService.RefreshSession();
+                results[testName] = new TestResult(result, sw);
             }
 
+            // END SESSION
             {
+                const string testName = nameof(AuthTest.EndSession);
                 sw.Restart();
-                var result = service.ChangePassword(player.ID, "abcdEE1234###£");
-                results[nameof(AuthTest.ChangePassword)] = new TestResult(result, sw);
+                var result = playerService.EndSession();
+                results[testName] = new TestResult(result, sw);
             }
 
+            // LIST NEWEST PLAYERS
             {
+                const string testName = nameof(AuthTest.ListNewestPlayers);
                 sw.Restart();
-                var result = service.ChangeUsername(player.ID, "Test" + new Random().Next(0, 10000));
-                results[nameof(AuthTest.ChangeUsername)] = new TestResult(result, sw);
+                var result = service.ListPlayers(
+                    new Players.ListPlayersOptions
+                    {
+                        Ordering = PlayerOrdering.Newest
+                    },
+                    new PaginationOptions(1, 20)
+                );
+                results[testName] = new TestResult(result, sw);
+                if (result.Success && result.Players.All(c => c.ID != player.ID))
+                {
+                    results[testName] = new TestResult(TestResultStatus.Failed, sw, "New player ID not found.");
+                }
             }
 
+            // LIST RECENTLY ACTIVE PLAYERS
             {
+                const string testName = nameof(AuthTest.ListMostRecentlyActivePlayers);
+                sw.Restart();
+                var result = service.ListPlayers(
+                    new Players.ListPlayersOptions
+                    {
+                        Ordering = PlayerOrdering.MostRecentlyActive
+                    },
+                    new PaginationOptions(1, 20)
+                );
+                results[testName] = new TestResult(result, sw);
+                if (result.Success && result.Players.All(c => c.ID != player.ID))
+                {
+                    results[testName] = new TestResult(TestResultStatus.Failed, sw, "New player ID not found.");
+                }
+            }
+
+            // GET PLAYER BY ID
+            {
+                const string testName = nameof(AuthTest.GetPlayerByID);
                 sw.Restart();
                 var result = service.GetPlayer(player.ID);
-                results[nameof(AuthTest.GetPlayer)] = new TestResult(result, sw);
+                results[testName] = new TestResult(result, sw);
+                if (result.Success && result.Player == null)
+                {
+                    results[testName] = new TestResult(TestResultStatus.Failed, sw);
+                }
             }
 
+            // GET PLAYER BY PLAYER NAME
             {
+                const string testName = nameof(AuthTest.GetPlayerByPlayerName);
+                sw.Restart();
+                var result = service.GetPlayer(playerName);
+                results[testName] = new TestResult(result, sw);
+                if (result.Success && result.Player == null)
+                {
+                    results[testName] = new TestResult(TestResultStatus.Failed, sw);
+                }
+            }
+
+            // CHANGE PLAYER NAME
+            var newPlayerName = "Test" + new Random().Next(0, 10000);
+            {
+                const string testName = nameof(AuthTest.ChangePlayerName);
+                sw.Restart();
+                var result = service.ChangePlayerName(player.ID, newPlayerName);
+                results[testName] = new TestResult(result, sw);
+            }
+
+            // GET PLAYER BY PLAYER NAME
+            {
+                const string testName = nameof(AuthTest.GetPlayerByUpdatedName);
+                sw.Restart();
+                var result = service.GetPlayer(newPlayerName);
+                results[testName] = new TestResult(result, sw);
+                if (result.Success && result.Player == null)
+                {
+                    results[testName] = new TestResult(TestResultStatus.Failed, sw);
+                }
+            }
+
+            // SET USERNAME/PASSWORD
+            {
+                const string testName = nameof(AuthTest.SetUsernamePassword);
+                sw.Restart();
+                var result = service.SetUsernameAndPassword(player.ID, "Test" + new Random().Next(0, 10000), "abc123#AB£");
+                results[testName] = new TestResult(result, sw);
+            }
+
+            // SET PASSWORD
+            {
+                const string testName = nameof(AuthTest.SetPassword);
+                sw.Restart();
+                var result = service.ChangePassword(player.ID, "abcdEE1234###£");
+                results[testName] = new TestResult(result, sw);
+            }
+
+            // SET USERNAME
+            {
+                const string testName = nameof(AuthTest.SetUsername);
+                sw.Restart();
+                var result = service.ChangeUsername(player.ID, "Test" + new Random().Next(0, 10000));
+                results[testName] = new TestResult(result, sw);
+            }
+            
+            // FORGOTTEN PASSWORD
+            {
+                const string testName = nameof(AuthTest.ForgottenPassword);
                 sw.Restart();
                 var result = service.RequestForgottenPasswordEmail("testemail@construct.ner");
-                results[nameof(AuthTest.ForgottenPassword)] = new TestResult(result, sw);
+                results[testName] = new TestResult(result, sw);
             }
 
+            // SET EMAIL ADDRESS
             {
+                const string testName = nameof(AuthTest.SetEmailAddress);
                 sw.Restart();
                 var result = service.SetEmailAddress(player.ID, "test+email@construct.net");
-                results[nameof(AuthTest.SetEmailAddress)] = new TestResult(result, sw);
+                results[testName] = new TestResult(result, sw);
             }
 
+            // SET RESTRICTIONS
             {
+                const string testName = nameof(AuthTest.SetRestrictions);
                 sw.Restart();
                 var result = service.SetPlayerRestrictions(player.ID, [PlayerRestriction.PlayerRateObjects]);
-                results[nameof(AuthTest.SetRestrictions)] = new TestResult(result, sw);
+                results[testName] = new TestResult(result, sw);
             }
 
+            // SET AVATAR BY URL
             {
+                const string testName = nameof(AuthTest.SetAvatarByURL);
                 sw.Restart();
-                var result = service.SetAvatar(player.ID,
-                    new PictureData(new Uri(
-                        "https://construct-static.com/images/v1746/r/uploads/user/15844/avatar/94145/avatar_v128.jpg",
-                        UriKind.Absolute)));
-                results[nameof(AuthTest.SetAvatar)] = new TestResult(result, sw);
+                var result = service.SetAvatar(
+                    player.ID,
+                    new PictureData(
+                        new Uri("https://construct-static.com/images/v1746/r/uploads/user/15844/avatar/94145/avatar_v128.jpg", UriKind.Absolute)
+                    )
+                );
+                results[testName] = new TestResult(result, sw);
             }
 
+            // SET AVATAR BY BASE 64
             {
+                const string testName = nameof(AuthTest.SetAvatarByBase64);
+                sw.Restart();
+                var result = service.SetAvatar(
+                    player.ID,
+                    new PictureData(Data.Base64ImageData)
+                );
+                results[testName] = new TestResult(result, sw);
+            }
+
+            // DELETE AVATAR
+            {
+                const string testName = nameof(AuthTest.DeleteAvatar);
                 sw.Restart();
                 var result = service.DeleteAvatar(player.ID);
-                results[nameof(AuthTest.DeleteAvatar)] = new TestResult(result, sw);
+                results[testName] = new TestResult(result, sw);
             }
 
+            // DELETE PLAYER
             {
+                const string testName = nameof(AuthTest.DeletePlayer);
                 sw.Restart();
                 var result = service.DeletePlayer(player.ID);
-                results[nameof(AuthTest.DeletePlayer)] = new TestResult(result, sw);
+                results[testName] = new TestResult(result, sw);
             }
         }
 
