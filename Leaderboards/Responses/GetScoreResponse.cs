4524d4625d6dda08ef7d5ef98d22f241fa7e0e76
Thomas Gullen <tom@scirra.com>
2026-03-12 10:53:49 +0000

    Add get score response object

---
+++ Leaderboards/Responses/GetScoreResponse.cs
@@ -0,0 +1,20 @@
+﻿using ConstructServices.Common;
+using ConstructServices.Leaderboards.Objects;
+using Newtonsoft.Json;
+
+namespace ConstructServices.Leaderboards.Responses
+{
+    public sealed class GetScoreResponse : BaseResponse
+    {
+        [JsonProperty(PropertyName = "score")]
+        public Score Score { get; set; }
+
+        public GetScoreResponse()
+        {
+        }
+        public GetScoreResponse(string errorMessage, bool shouldRetry = false) : base(errorMessage, shouldRetry)
+        {
+
+        }
+    }
+}
